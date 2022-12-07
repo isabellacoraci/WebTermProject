43c656cca70113f3331aa318fd9627806d881220
isabellacoraci <ic21353n@pace.edu>
2022-12-06 19:18:24 -0500

    12-6 final commit

---
+++ assets/css/scripts.js
@@ -0,0 +1,5 @@
+function darkMode() {
+	document.querySelector("body").style.backgroundColor="black"
+    document.querySelector("main").style.color="white"
+    document.querySelector("h1").style.color = "white"
+}
