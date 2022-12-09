8cdeee237d69781247d65f6e8cc56695186fd7b6
isabellacoraci <ic21353n@pace.edu>
2022-12-08 20:13:01 -0500

    12-8 commit 2

--- assets/css/scripts.js
+++ assets/css/scripts.js
@@ -1,4 +1,4 @@
 function darkMode() {
-	document.querySelector("body").style.backgroundColor="navy"
+	document.querySelector("body").style.backgroundColor="black"
     document.querySelector("body").style.color="white"
 }
