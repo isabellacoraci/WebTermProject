6c9344a51dccd428793c7ad799d75535fe3c921e
isabellacoraci <ic21353n@pace.edu>
2022-12-12 16:32:40 -0500

    12-12 commit 4

--- assets/css/scripts.js
+++ assets/css/scripts.js
@@ -2,3 +2,18 @@ function darkMode() {
 	document.querySelector("body").style.backgroundColor="black"
     document.querySelector("body").style.color="white"
 }
+
+let btn = document.forms.signinForm.submit
+let firstemail = document.forms.signinForm.firstemail
+let secondemail = document.forms.signinForm.secondemail
+let errs = document.querySelector("#errors")
+
+btn.addEventListener("click", validatEmails)
+
+function validateEmails(event) {
+    if(firstemail.value != secondemail.value) {
+        let msg = "Your emails don't match."
+        errs.innerHTML = msg
+        event.preventDefault()
+    }
+}
